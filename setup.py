14e152280a029113d24f8a3479eae914ba0908d0
Steve Brettschneider <steve@bluehousefamily.com>
2020-04-19 16:53:36 -0400

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'dict-wrapper',
+    py_modules = ['dict_wrapper'],
+    version = '0.1',
+    description = 'Wraps python dictionary keys are attributes',
+    author = 'Steve Brettschneider',
+    author_email = 'steve@bluehousefamily.com',
+    url = 'https://github.com/brettschneider/python_dict_wrapper',
+    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.1.tar.gz',
+    keywords = ['dictionary', 'wrapper', 'attributes', 'enforce'],
+    classifiers = []
+)
