d3463bc503092992db1453715c7862b8e4f6c289
Steve Brettschneider <steve@bluehousefamily.com>
2020-04-19 18:47:36 -0400

    Working on pypi.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,5 +2,9 @@
 python-dict-wrapper changelog
 =============================
 
+**2020-04-19** - Release 0.3 - Initial release.
+
+**2020-04-19** - Release 0.2 - Initial release.
+
 **2020-04-19** - Release 0.1 - Initial release.
 
--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@
 
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'python-dict-wrapper',
-    py_modules = ['dict_wrapper'],
-    version = '0.1',
+    py_modules = ['python_dict_wrapper'],
+    version = '0.3',
     description = 'Wraps python dictionary keys are attributes',
+    long_description=long_description,
     author = 'Steve Brettschneider',
     author_email = 'steve@bluehousefamily.com',
     url = 'https://github.com/brettschneider/python_dict_wrapper',
-    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.1.tar.gz',
+    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.2.tar.gz',
     keywords = ['dictionary', 'wrapper', 'attributes', 'enforce'],
     classifiers = []
 )
